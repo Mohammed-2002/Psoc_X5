dca778c4f451394161ef7031f8bd4acc11684bea
JohannesChopov <johannes.chopov@student.uhasselt.be>
2024-12-10 17:07:12 +0100

    na cleanup

--- MQTT_UDP_client/source/global_variables.h
+++
@@ -1,9 +0,0 @@
-// global_variables.h
-#ifndef GLOBAL_VARIABLES_H
-#define GLOBAL_VARIABLES_H
-
-#include <stdbool.h>  // Include for using boolean type
-
-extern bool wifi_connected;  // Declare the global variable
-
-#endif // GLOBAL_VARIABLES_H
